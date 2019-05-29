3c8defc927f20f466576556ae6552d4ad52abaea
Stephen Rosso <stephenprosso@gmail.com>
2019-05-29 08:45:19 -0700

    div update in index.cshtml

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevExpress.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,6 +27,80 @@ namespace ComicBookGallery.Controllers
             return View();
 
 
+        }
+
+        ComicBookGallery.Models.RexEntities db = new ComicBookGallery.Models.RexEntities();
+
+        [ValidateInput(false)]
+        public ActionResult GridViewPartial()
+        {
+            var model = db.ORDER_DETAIL;
+            return PartialView("~/Views/Home/_GridViewPartial.cshtml", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridViewPartialAddNew([ModelBinder(typeof(DevExpressEditorsBinder))] ComicBookGallery.Models.ORDER_DETAIL item)
+        {
+            var model = db.ORDER_DETAIL;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Add(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("~/Views/Home/_GridViewPartial.cshtml", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridViewPartialUpdate([ModelBinder(typeof(DevExpressEditorsBinder))] ComicBookGallery.Models.ORDER_DETAIL item)
+        {
+            var model = db.ORDER_DETAIL;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var modelItem = model.FirstOrDefault(it => it.Order_Number == item.Order_Number);
+                    if (modelItem != null)
+                    {
+                        this.UpdateModel(modelItem);
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("~/Views/Home/_GridViewPartial.cshtml", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridViewPartialDelete(System.String Order_Number)
+        {
+            var model = db.ORDER_DETAIL;
+            if (Order_Number != null)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.Order_Number == Order_Number);
+                    if (item != null)
+                        model.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            return PartialView("~/Views/Home/_GridViewPartial.cshtml", model.ToList());
         }
     }
 }
