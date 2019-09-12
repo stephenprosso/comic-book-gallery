df93458db90b182ec52f272d0fca2571755226ca
Stephen Rosso <stephenprosso@gmail.com>
2019-09-12 11:27:48 -0700

    added multiple comics to the comicbook respository

---
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        //bye making this field static we are asociating it with the comicBookRepository class instead of the individual object instances of this class.
+        //this will ensure we only have 1 copy of the comic book data in memory at any given time
+        private static ComicBook[] _comicBook = new ComicBook[] {
+
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
+                }
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
+                }
+            }
+
+        };
+
+
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+            //foreach is a code snippet. type foreach and hit tabe twice and it will populate code with the needed inputs 
+            foreach (var comicBook in _comicBook)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+
+                    break;
+                }
+            }
+
+            return comicBookToReturn;
+        }
+    }
+}
